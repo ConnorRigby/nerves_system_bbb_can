43f3829b27735e7215d72b595270051583fceb77
Connor Rigby <connorr@hey.com>
2022-03-25 17:54:51 -0600

    Use FIT image for kernel and device tree

--- post-createfs.sh
+++ post-createfs.sh
@@ -4,5 +4,9 @@ set -e
 
 FWUP_CONFIG=$NERVES_DEFCONFIG_DIR/fwup.conf
 
+# Create the itb file for U-Boot
+cp "$NERVES_DEFCONFIG_DIR/uboot/kernel.its" "$BINARIES_DIR"
+(cd "$BINARIES_DIR" && ../host/bin/mkimage -f kernel.its kernel.itb)
+
 # Run the common post-image processing for nerves
 $BR2_EXTERNAL_NERVES_PATH/board/nerves-common/post-createfs.sh $TARGET_DIR $FWUP_CONFIG
